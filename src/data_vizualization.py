021c9c3459dd6144d5e10704a08245c33f62c847
SabiSasu <sabina.sasu@gmail.com>
2020-10-12 21:05:18 -0400

    changed file used to interpolated, added graphs to predict PCA components and number of clusters

--- src/data_vizualization.py
+++ src/data_vizualization.py
@@ -2,9 +2,10 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
+from sklearn.cluster import KMeans
 from datetime import date, timedelta
 
-merged_data = pd.read_csv('../data/merged_data.csv')
+merged_data = pd.read_csv('../data/interpolated_merged_data.csv')
 merged_data=merged_data.fillna(0)
 chosen_symptoms=['symptom:Cough', 'symptom:Common cold' , 'symptom:Fever', 'symptom:Pneumonia']
 
@@ -22,13 +23,15 @@ date_data = by_state.get_group(region_names[0]).sort_values(by=['date'])['date']
 print(merged_data.columns[7:21])
 #display symptom search per region per week
 #temporarily displays one plot at a time, but will be grouped up later
-for sym_index, symptom_name in enumerate(chosen_symptoms):
+#for sym_index, symptom_name in enumerate(chosen_symptoms):
+for sym_index, symptom_name in enumerate(symptoms_list):
     #continue
     colors = 'b', 'g', 'r', 'c', 'm', 'y', 'k'
     width = 0.8
     bottoms = np.array([0])
     tempI=0
     colIndex=0
+    #plt.subplot(len(chosen_symptoms), 1, sym_index+1)
     for index, region_name in enumerate(region_names):
 
         temp = by_state.get_group(region_name).sort_values(by=['date'])[symptom_name]
@@ -49,55 +52,92 @@ for sym_index, symptom_name in enumerate(chosen_symptoms):
 
         if colIndex >= len(colors):
             colIndex=0
-        plt.bar(date_data, temp, width, bottom=bottoms, color=colors[colIndex])
-        bottoms = np.add(bottoms, np.array(temp))
-        colIndex+=1
+        if symptom_name in chosen_symptoms:
+            plt.bar(date_data, temp, width, bottom=bottoms, color=colors[colIndex])
+            bottoms = np.add(bottoms, np.array(temp))
+            colIndex+=1
 
-    plt.title(symptom_name)
-    plt.xticks(rotation=90)
-    plt.legend(labels=region_names)
-    plt.show()
-    #break
-
+    if symptom_name in chosen_symptoms:
+        plt.title(symptom_name)
+        plt.xticks(rotation=90)
+        plt.legend(labels=region_names)
+        plt.show()
+       #break
 
 #PCA
-
+#get only columns with symptoms
 X=merged_data[merged_data.columns[3:len(symptoms_list)+3]]
 print(X)
-#print(iris.data)
-# Time to get them tools --> import and initialize
-from sklearn.decomposition import PCA
-pca = PCA(n_components=len(symptoms_list))
+#Figure out how many components (kinda useless)
+pca2 = PCA()
+pca2.fit(X)
+num_pc_components = len(pca2.explained_variance_ratio_)
+plt.subplot(2,1,1)
+plt.plot(np.linspace(1,num_pc_components,num_pc_components),100*pca2.explained_variance_ratio_)
+plt.xlabel("Principal Component #")
+plt.ylabel("% Variance explained")
+plt.title("Variance Explained vs Principal Components")
+
+plt.subplot(2,1,2)
+plt.plot(np.linspace(1,num_pc_components,num_pc_components),100*np.cumsum(pca2.explained_variance_ratio_))
+plt.plot(np.linspace(1,num_pc_components,num_pc_components),95*np.ones((num_pc_components,)),'k--')
+plt.xlabel("Principal Component #")
+plt.ylabel("% Variance explained")
+plt.title("Cumulative Variance Explained vs Principal Components")
+plt.show()
+
+
+
+
+
+
+#PCA graph
+markersize=4
+pca = PCA(n_components=3)
 pca.fit(X)
 X_reduced = pca.transform(X)
-
-# Let's make a scatter plot as before for this reduced dimension Iris data
-# this formatter will label the colorbar with the correct target names
-#formatter = plt.FuncFormatter(lambda i, *args: iris.target_names[int(i)])
-
-#plt.scatter(X_reduced[:,0], X_reduced[:,1], c=y, cmap=plt.cm.get_cmap('viridis',3))
-plt.scatter(X_reduced[:,0], X_reduced[:,1])
-#plt.colorbar(ticks=[0,1,2], format=formatter)
+plt.scatter(X_reduced[:,0], X_reduced[:,1], s=markersize)
 plt.clim(-0.5,2.5)
 plt.xlabel("PC #1")
 plt.ylabel("PC #2")
 plt.show()
 
 
+ks = range(1, 10)
+inertias = []
+for k in ks:
+    # Create a KMeans instance with k clusters: model
+    model = KMeans(n_clusters=k)
+
+    # Fit model to samples
+    model.fit(X_reduced)
+
+    # Append the inertia to the list of inertias
+    inertias.append(model.inertia_)
+
+plt.plot(ks, inertias, '-o', color='black')
+plt.xlabel('number of clusters, k')
+plt.ylabel('inertia')
+plt.xticks(ks)
+plt.show()
+
+
 #Clusters
-from sklearn.cluster import KMeans
-kmeans_high = KMeans(n_clusters=3, random_state=0)
+clusterNum=4
+#high is raw data
+kmeans_high = KMeans(n_clusters=clusterNum, random_state=0)
 kmeans_high.fit(X)
 y_pred_high = kmeans_high.predict(X)
 
-kmeans_low = KMeans(n_clusters=3, random_state=0)
+#low is reduced dimensionality data
+kmeans_low = KMeans(n_clusters=clusterNum, random_state=0)
 kmeans_low.fit(X_reduced)
 y_pred_low = kmeans_low.predict(X_reduced)
 
 # Plot 3 scatter plots -- two for high and low dimensional clustering results and one indicating the ground truth labels
 
 plt.subplot(3,1,1)
-plt.scatter(X_reduced[:,0], X_reduced[:,1], c=y_pred_high, cmap=plt.cm.get_cmap('viridis',3))
+plt.scatter(X_reduced[:,0], X_reduced[:,1], s=markersize, c=y_pred_high)
 plt.colorbar(ticks=[0,1,2])
 plt.clim(-0.5,2.5)
 plt.xlabel("PC #1")
@@ -105,7 +145,7 @@ plt.ylabel("PC #2")
 plt.title("Cluster labels for high-dimensional KMeans")
 
 plt.subplot(3,1,2)
-plt.scatter(X_reduced[:,0], X_reduced[:,1], c=y_pred_low, cmap=plt.cm.get_cmap('viridis',3))
+plt.scatter(X_reduced[:,0], X_reduced[:,1], s=markersize, c=y_pred_low)
 plt.colorbar(ticks=[0,1,2])
 plt.clim(-0.5,2.5)
 plt.xlabel("PC #1")
@@ -114,7 +154,7 @@ plt.title("Cluster labels for low-dimensional KMeans")
 
 plt.subplot(3,1,3)
 #plt.scatter(X_reduced[:,0], X_reduced[:,1], c=y, cmap=plt.cm.get_cmap('viridis',3))
-plt.scatter(X_reduced[:,0], X_reduced[:,1], cmap=plt.cm.get_cmap('viridis',3))
+plt.scatter(X_reduced[:,0], X_reduced[:,1], s=markersize)
 #plt.colorbar(ticks=[0,1,2], format=formatter)
 plt.clim(-0.5,2.5)
 plt.xlabel("PC #1")
